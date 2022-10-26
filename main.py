0277edbbd61b7f0b125495d5fe36db8392a01127
Zexian <zexianlie@gmail.com>
2022-10-26 12:26:24 -0700

    Drop rows where button was pressed

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ data = pd.read_csv("data/17/dend000.csv", header=[0, 1])
 data = data.rename(
     columns={'Unnamed: 1_level_0': 'ID', 'Unnamed: 3_level_0': 'Timestamp', 'Unnamed: 7_level_0': 'SHT31D'})
 
-# Drop last column
+# Drop last column (it is empty)
 data = data.iloc[:, :-1]
 
-
-print(data.head())
-
-
+# Drop rows where button was pressed
 indexesToDrop = data[data[('Button', 'Pressed?')] == 1].index
-print(indexesToDrop)
-data = data.drop(indexesToDrop, inplace=True)
-
-
-print(data)
+data.drop(indexesToDrop, inplace=True)
+data.reset_index(drop=True, inplace=True)
