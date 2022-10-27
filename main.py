a0a4bc50f5ccfe3d66ef0ac21c8dafa4fbc9879a
Zexian <zexianlie@gmail.com>
2022-10-26 17:07:20 -0700

    Dropped rows where status code is not green nor yellow

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from operator import index
+from time import process_time_ns
 import pandas as pd
 
 # Read data
-data = pd.read_csv("data/17/dend000.csv", header=[0, 1])
+data = pd.read_csv("data/18/dend00.csv", header=[0, 1])
 
 # Change column names
 data = data.rename(
@@ -11,6 +13,15 @@ data = data.rename(
 data = data.iloc[:, :-1]
 
 # Drop rows where button was pressed
-indexesToDrop = data[data[('Button', 'Pressed?')] == 1].index
-data.drop(indexesToDrop, inplace=True)
+idxBtnPressed = data[data[('Button', 'Pressed?')] == 1].index
+data.drop(idxBtnPressed, inplace=True)
 data.reset_index(drop=True, inplace=True)
+print(f"-- Dropped {idxBtnPressed.shape[0]} rows where button was pressed.")
+
+# Drop rows where status color was neither green nor yellow.
+idxColorStatus = data[(data[('Status', 'Color')] != "Green") &
+                      (data[('Status', 'Color')] != "Yellow")].index
+data.drop(idxColorStatus, inplace=True)
+data.reset_index(drop=True, inplace=True)
+print(
+    f"-- Dropped {idxColorStatus.shape[0]} rows where status color was neither green nor yellow.")
