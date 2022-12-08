e3db669f852cace55af2d1e2c2437ef673906deb
Zexian <zexianlie@gmail.com>
2022-12-07 16:25:12 -0800

    Change to only correct over/under flow

--- main.py
+++ main.py
@@ -22,34 +22,26 @@ df.insert(2, "Time", date_time_combined)
 
 original = df.copy(deep=True)
 
-# Drop rows where button was pressed
-idxBtnPressed = df[df[('Button', 'Pressed?')] == 1].index
-df.drop(idxBtnPressed, inplace=True)
-df.reset_index(drop=True, inplace=True)
-print(f"-- Dropped {idxBtnPressed.shape[0]} rows where button was pressed.")
+# # Drop rows where button was pressed
+# idxBtnPressed = df[df[('Button', 'Pressed?')] == 1].index
+# df.drop(idxBtnPressed, inplace=True)
+# print(f"-- Dropped {idxBtnPressed.shape[0]} rows where button was pressed.")
 
-# Drop rows where status color was neither green nor yellow.
-idxColorStatus = df[(df[('Status', 'Color')] != "Green") &
-                    (df[('Status', 'Color')] != "Yellow")].index
-df.drop(idxColorStatus, inplace=True)
-df.reset_index(drop=True, inplace=True)
-print(
-    f"-- Dropped {idxColorStatus.shape[0]} rows where status color was neither green nor yellow.")
+# # Drop rows where status color was neither green nor yellow.
+# idxColorStatus = df[(df[('Status', 'Color')] != "Green") &
+#                     (df[('Status', 'Color')] != "Yellow")].index
+# df.drop(idxColorStatus, inplace=True)
+# print(
+#     f"-- Dropped {idxColorStatus.shape[0]} rows where status color was neither green nor yellow.")
 
-
+# df.reset_index(drop=True, inplace=True)
 data = df.copy(deep=True)
 
-# print(data.iloc[960:1100][('AS5311', 'Serial_Value')])
-
-# print(data.head)
-
-"""
-Remove entries where the change in displacement is greater or equal to
-"""
 toRemove = []
 prevIdx, prevSerial = 0, data.iloc[0][('AS5311', 'Serial_Value')]
 initial, calculated, wrap = data.iloc[0][('AS5311', 'Serial_Value')], 0, 0
-out_of_while = 0
+data.at[0, 'Calculated'] = 0
+
 curIdx = 1
 while curIdx < data.shape[0]:
     curSerial = data.iloc[curIdx][('AS5311', 'Serial_Value')]
@@ -59,81 +51,35 @@ while curIdx < data.shape[0]:
 
     curCalcSerial = prevSerial + wrap - initial
 
-    """
-    We place a pointer at the current row and start the loop.
-    The loop will keep going until it finds a displacement that is relatively close to the refence.
-
-    This could be changed to number of entries we remove before backing out
-    since the data is collected every ~15 minutes
-    """
-    overrides = 0
-    # Increased the number of overrides from 30 -> 100
-    while abs(change) >= 250 and abs(change) <= 3800 and curIdx + 1 < data.shape[0] and overrides < 30:
-
-        data.at[curIdx, ('AS5311', 'Serial_Value')] = prevSerial
-        prevCalculated = float(data.iloc[prevIdx][('Calculated')])
-
-        data.at[curIdx, ('Calculated')] = prevCalculated
-
-        curIdx = curIdx + 1
-        overrides += 1
-        curSerial = data.iloc[curIdx][('AS5311', 'Serial_Value')]
-
-        change = (curSerial - prevSerial)
-
     if abs(change) > 3800:
         # Wrap up
         if change < 0:
-            # print("-- Wrapped up", prevSerial, curSerial, change)
+            print("-- Wrapped up", prevSerial, curSerial, change)
             wrap += 4095
 
         # Wrap down
         elif change > 0:
-            # print("-- Wrapped down", prevSerial, curSerial, change)
+            print("-- Wrapped down", prevSerial, curSerial, change)
             wrap -= 4095
 
         # Calculate the displacement data
         curCalcSerial = curSerial + wrap - initial
 
-        # Data to use in the plot
-        data.at[curIdx, 'Calculated'] = curCalcSerial
-
-        # prevSerial = curSerial
-        # prevIdx = curIdx
-        # curIdx += 1
-    else:
-        # Data to use in the plot
-        data.at[curIdx, 'Calculated'] = curCalcSerial
-        # prevSerial = curSerial
-        # prevIdx = curIdx
-        # curIdx += 1
+    # Data to use in the plot
+    data.at[curIdx, 'Calculated'] = curCalcSerial
 
     prevSerial = curSerial
     prevIdx = curIdx
     curIdx += 1
 
-print(data.iloc[960:1100][('AS5311', 'Serial_Value')],
-      data.iloc[960:1100][('Calculated')])
-# print(data.iloc[500:540][('AS5311', 'Serial_Value')], "\n",
-#       data.iloc[500:540][('Change')])
-# print(data.head(10))
-print(toRemove)
-
 # Plot stuff
 fig1 = plt.subplot()
-fig1.plot(original["Time"], original[('AS5311', 'Serial_Value')])
 fig1.plot(df["Time"], df[('AS5311', 'Serial_Value')])
 fig1.plot(data["Time"], data['Calculated'])
-fig1.legend(['Original', 'Simple clean', 'Calculated'])
+# fig1.legend(['Original', 'Simple clean', 'Calculated'])
+fig1.legend(['Simple clean', 'Calculated'])
 fig1.set_xlabel("Time")
 fig1.set_ylabel("Displacement")
 # fig1.set_xticklabels([])
 plt.show()
 plt.close()
-
-
-# fig2 = plt.subplot()
-# fig2.hist(data['Change'], bins=100)
-# plt.xlim([-500, 500])
-# plt.yscale('log')
-# plt.show()
