245af496187442ededa2a3333e574e624c2e1754
Zexian Li <55903183+zexianli@users.noreply.github.com>
2022-12-19 18:23:37 -0800

    Simple over/under flow correction (#1)
    * Check for serial data wrap up and down
    
    * Clean data and plot it
    
    * Ignore vscode workspace
    
    * Progress fixing wrapping algo
    
    * Fixed data spikes
    
    * Prevent overriding entire set of data
    
    * Changed from fixed values to rate of change for spike detection
    
    * Fix ignore small change
    
    * Working on fixing jumping
    
    * Some progress on graph flatline fix
    
    * Change to only correct over/under flow
    
    * Testing over/under flow
    
    * Save plots into a file

--- main.py
+++ main.py
@@ -1,27 +1,69 @@
-from operator import index
-from time import process_time_ns
 import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.options.display.max_rows = 200
+
 
 # Read data
-data = pd.read_csv("data/18/dend00.csv", header=[0, 1])
+df = pd.read_csv("data/28/dend00.csv", header=[0, 1])
 
 # Change column names
-data = data.rename(
+df = df.rename(
     columns={'Unnamed: 1_level_0': 'ID', 'Unnamed: 3_level_0': 'Timestamp', 'Unnamed: 7_level_0': 'SHT31D'})
 
 # Drop last column (it is empty)
-data = data.iloc[:, :-1]
+df = df.iloc[:, :-1]
 
-# Drop rows where button was pressed
-idxBtnPressed = data[data[('Button', 'Pressed?')] == 1].index
-data.drop(idxBtnPressed, inplace=True)
-data.reset_index(drop=True, inplace=True)
-print(f"-- Dropped {idxBtnPressed.shape[0]} rows where button was pressed.")
+# print(df.head())
+date_time_combined = pd.to_datetime(
+    df[("Timestamp", "date")] + ' ' + df[("Timestamp", "time")])
+df.drop(columns=[("Timestamp", "date"), ("Timestamp", "time")], inplace=True)
+df.insert(2, "Time", date_time_combined)
 
-# Drop rows where status color was neither green nor yellow.
-idxColorStatus = data[(data[('Status', 'Color')] != "Green") &
-                      (data[('Status', 'Color')] != "Yellow")].index
-data.drop(idxColorStatus, inplace=True)
-data.reset_index(drop=True, inplace=True)
-print(
-    f"-- Dropped {idxColorStatus.shape[0]} rows where status color was neither green nor yellow.")
+data = df.copy(deep=True)
+
+toRemove = []
+prev_idx, prev_serial = 0, data.iloc[0][('AS5311', 'Serial_Value')]
+initial, calculated, wrap = data.iloc[0][('AS5311', 'Serial_Value')], 0, 0
+data.at[0, 'Calculated'] = 0
+
+cur_idx = 1
+while cur_idx < data.shape[0]:
+    cur_serial = data.iloc[cur_idx][('AS5311', 'Serial_Value')]
+
+    change = (cur_serial - prev_serial)/(cur_idx - prev_idx)
+    data.at[cur_idx, 'Change'] = change
+
+    calculated_serial_data = prev_serial + wrap - initial
+
+    if abs(change) > 2000:
+        # Wrap up
+        if change < 0:
+            print("-- Wrapped up", prev_serial, cur_serial, change)
+            wrap += 4095
+
+        # Wrap down
+        elif change > 0:
+            print("-- Wrapped down", prev_serial, cur_serial, change)
+            wrap -= 4095
+
+        # Calculate the displacement data
+        calculated_serial_data = cur_serial + wrap - initial
+
+    # Data to use in the plot
+    data.at[cur_idx, 'Calculated'] = calculated_serial_data
+
+    prev_serial = cur_serial
+    prev_idx = cur_idx
+    cur_idx += 1
+
+# Plot stuff
+fig1 = plt.subplot()
+fig1.set_title("Dendrometer #1")
+fig1.plot(df["Time"], df[('AS5311', 'Serial_Value')])
+fig1.plot(data["Time"], data['Calculated'])
+fig1.legend(['Raw data', 'Over/Under flow adjusted'])
+fig1.set_xlabel("Time")
+fig1.set_ylabel("Displacement (serial value)")
+# fig1.set_xticklabels([])
+plt.show()
