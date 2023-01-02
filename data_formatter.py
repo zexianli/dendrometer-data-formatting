fc81df8512fe5d43ed0c0287e9052716f8a58084
Zexian <zexianlie@gmail.com>
2023-01-02 12:36:27 -0800

    read the time of each data file

--- data_formatter.py
+++ data_formatter.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 from collections import defaultdict
+import linecache
 
 
 class DataFormatter:
@@ -19,7 +20,7 @@ class DataFormatter:
         cur_path = Path.cwd()
         data_path = Path.joinpath(cur_path, "data")
         folders = [folder for folder in data_path.iterdir() if folder.is_dir()]
-        self.folder_file_map = defaultdict(list)
+        folder_file_map = defaultdict(list)
 
         for folder in folders:
             files_path = Path.joinpath(data_path, folder)
@@ -28,19 +29,30 @@ class DataFormatter:
             for file in files:
                 file_size = Path(Path.joinpath(
                     files_path, file)).stat().st_size
-                print(file_size)
 
                 if file_size > min_file_size:
-                    self.folder_file_map[folder].append(file)
+                    folder_file_map[folder].append(file)
 
-        return self.folder_file_map
+        return folder_file_map
 
+    def get_time_differences(self, folders: dict):
+        times = []
+        for files in folders.values():
+            for file in files:
+                first_entry = linecache.getline(Path.as_posix(file), 3)
+                linecache.clearcache()
+
+                time_entry = first_entry.split(",")[2:4]
+                cur_time = pd.to_datetime(time_entry[0] + " " + time_entry[1])
+                print(cur_time)
+                times.append(cur_time)
+
+        return times
 
 def main():
     formatter = DataFormatter()
     files = formatter.find_valid_files()
-    for folder, file in files.items():
-        print(folder, ": ", file)
+    formatter.get_time_differences(files)
 
 
 if __name__ == "__main__":
