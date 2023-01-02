3e65cea6d78b3a66b4d67c3310a9077f48452454
Zexian <zexianlie@gmail.com>
2023-01-02 13:30:57 -0800

    quick fix to plot all files

--- data_formatter.py
+++ data_formatter.py
@@ -49,11 +49,91 @@ class DataFormatter:
 
         return times
 
+    def iter_files(self, folders: dict):
+        for folder in folders.keys():
+            print(folder)
+            for file in folders[folder]:
+                df = pd.read_csv(Path.as_posix(file), header=[0, 1])
+                self.create_plot(df, file)
+
+    def create_plot(self, df: pd.DataFrame, filename):
+        # Change column names
+        df = df.rename(
+            columns={'Unnamed: 1_level_0': 'ID', 'Unnamed: 3_level_0': 'Timestamp', 'Unnamed: 7_level_0': 'SHT31D'})
+
+        # Drop last column (it is empty)
+        df = df.iloc[:, :-1]
+
+        date_time_combined = pd.to_datetime(
+            df[("Timestamp", "date")] +
+            ' ' +
+            df[("Timestamp", "time")])
+
+        df.drop(columns=[
+                ("Timestamp", "date"),
+                ("Timestamp", "time")], inplace=True)
+
+        df.insert(2, "Time", date_time_combined)
+
+        data = df.copy(deep=True)
+
+        toRemove = []
+
+        prev_idx, prev_serial = 0, data.iloc[0][('AS5311', 'Serial_Value')]
+        initial, calculated, wrap = data.iloc[0][(
+            'AS5311', 'Serial_Value')], 0, 0
+        data.at[0, 'Calculated'] = 0
+
+        cur_idx = 1
+        while cur_idx < data.shape[0]:
+            cur_serial = data.iloc[cur_idx][('AS5311', 'Serial_Value')]
+
+            change = (cur_serial - prev_serial)/(cur_idx - prev_idx)
+            data.at[cur_idx, 'Change'] = change
+
+            calculated_serial_data = prev_serial + wrap - initial
+
+            if abs(change) > 2000:
+                # Wrap up
+                if change < 0:
+                    print("-- Wrapped up", prev_serial, cur_serial, change)
+                    wrap += 4095
+
+                # Wrap down
+                elif change > 0:
+                    print("-- Wrapped down", prev_serial, cur_serial, change)
+                    wrap -= 4095
+
+            # Calculate the displacement data
+            calculated_serial_data = cur_serial + wrap - initial
+
+            # Data to use in the plot
+            data.at[cur_idx, 'Calculated'] = calculated_serial_data
+
+            prev_serial = cur_serial
+            prev_idx = cur_idx
+            cur_idx += 1
+
+        filename = filename.with_suffix('')
+        dend_file_name = str(filename).split("/")[-2:]
+        plot_title = f"Dendrometer_{dend_file_name[0]}_{dend_file_name[1]}"
+
+        plt.figure(dpi=600, figsize=(11.69, 8.27))
+        fig1 = plt.subplot()
+        fig1.set_title(plot_title)
+        fig1.plot(df["Time"], df[('AS5311', 'Serial_Value')])
+        fig1.plot(data["Time"], data['Calculated'])
+        fig1.legend(['Raw data', 'Over/Under flow adjusted'])
+        fig1.set_xlabel("Time")
+        fig1.set_ylabel("Displacement (serial value)")
+        plt.savefig("plots/" + plot_title + ".pdf")
+
+
 def main():
     formatter = DataFormatter()
     files = formatter.find_valid_files()
     formatter.get_time_differences(files)
-
+    formatter.iter_files(files)
 
 if __name__ == "__main__":
     main()
